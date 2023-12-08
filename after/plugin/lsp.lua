2360922c0dd51e7b03b84c97328a6ccf5a430eb0
Daniel Barde <mrbarde2k@gmail.com>
2023-12-08 23:29:02 +0100

    feat: add lsp support

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,38 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+	-- lsp.default_keymaps({buffer = bufnr})
+
+	local opts = {buffer = bufnr, remap = false}
+
+	vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+	vim.keymap.set('n', '<leader>vd', function() vim.lsp.buf.open_float() end, opts)
+	vim.keymap.set('n', '[d', function() vim.lsp.buf.goto_next() end, opts)
+	vim.keymap.set('n', ']d', function() vim.lsp.buf.goto_prev() end, opts)
+	vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set('n', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  -- Replace the language servers listed here 
+  -- with the ones you want to install
+  ensure_installed = {
+    'tsserver',
+    'eslint',
+    'rust_analyzer',
+    'gopls',
+    'swift_mesonls',
+    'html',
+    'cssls'
+  },
+  handlers = {
+    lsp.default_setup,
+  },
+})
+
+lsp.setup()
--- lua/thecodecafe/packer.lua
+++ lua/thecodecafe/packer.lua
@@ -17,10 +17,31 @@ return require('packer').startup(function(use)
 		as = 'rose-pine',
 		config = function()
 			vim.cmd('colorscheme rose-pine')
-	        end
-      })
+		end
+	})
 
-      use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-      use('mbbill/undotree')
-      use('tpope/vim-fugitive')
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			--- Uncomment these if you want to manage LSP servers from neovim
+			-- {'williamboman/mason.nvim'},
+			-- {'williamboman/mason-lspconfig.nvim'},
+
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	}
+	use {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	}
 end)
