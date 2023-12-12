80facd06d2ea4d64d496b8e427aa84e9f4b12bc7
Daniel Barde <mrbarde2k@gmail.com>
2023-12-12 11:39:12 +0100

    feat(fugitive.lua): add git push command

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,8 +2,10 @@ vim.keymap.set('n', '<leader>gs', vim.cmd.Git);
 
 vim.keymap.set('n', '<leader>ga', ":Git add ")
 
-vim.keymap.set('n', '<leader>gc', ":Git commit ")
+vim.keymap.set('n', '<leader>gc', ":Git commit -m ")
 
 vim.keymap.set('n', '<leader>gm', function()
   vim.cmd(":Git mergetool")
 end)
+
+vim.keymap.set('n', '<leader>gp', ":Git push")
