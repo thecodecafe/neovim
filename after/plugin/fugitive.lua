9c82fb9b9cc647366bfcb3207ecfec8d149649b4
Daniel Barde <mrbarde2k@gmail.com>
2023-12-12 11:34:34 +0100

    feat(fugitive.lua): add more git commands

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,9 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git);
+
+vim.keymap.set('n', '<leader>ga', ":Git add ")
+
+vim.keymap.set('n', '<leader>gc', ":Git commit ")
+
+vim.keymap.set('n', '<leader>gm', function()
+  vim.cmd(":Git mergetool")
+end)
