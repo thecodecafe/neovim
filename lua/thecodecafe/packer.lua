7e75e0028a1d26ae5ab61df3e490677fdc473f5b
Daniel Barde <mrbarde2k@gmail.com>
2024-02-24 12:10:30 +0100

    updates

--- lua/thecodecafe/packer.lua
+++ lua/thecodecafe/packer.lua
@@ -3,13 +3,13 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    -- or                            , branch = '0.1.x',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 
   use({
     "nvim-treesitter/nvim-treesitter-textobjects",
@@ -17,50 +17,50 @@ return require('packer').startup(function(use)
     requires = "nvim-treesitter/nvim-treesitter",
   })
 
-  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    requires = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'L3MON4D3/LuaSnip' },
-    }
-  }
-  use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-  }
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	}
+	use {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	}
   use({
     "folke/tokyonight.nvim",
     as = "tokyonight"
   })
-  use { "rebelot/kanagawa.nvim", as = "kanagawa" }
+  use {"rebelot/kanagawa.nvim", as = "kanagawa"}
   use {
     "smjonas/inc-rename.nvim",
     config = function()
       require("inc_rename").setup()
     end,
   }
-  use { 'stevearc/dressing.nvim' }
+  use {'stevearc/dressing.nvim'}
   use {
     'hrsh7th/nvim-cmp',
     requires = {
-      { 'hrsh7th/cmp-buffer' },         -- source for text in buffer
-      { 'hrsh7th/cmp-path' },           -- source for file system path
-      { 'L3MON4D3/LuaSnip' },           -- snippet engine
-      { 'saadparwaiz1/cmp_luasnip' },   -- for autocompletoin
-      { 'rafamadriz/friendly-snippets' }, -- usefull snippets
-      { "onsails/lspkind.nvim" }        -- vs-code like pictograms
+      {'hrsh7th/cmp-buffer'}, -- source for text in buffer
+      {'hrsh7th/cmp-path'}, -- source for file system path
+      {'L3MON4D3/LuaSnip'}, -- snippet engine
+      {'saadparwaiz1/cmp_luasnip'}, -- for autocompletoin
+      {'rafamadriz/friendly-snippets'}, -- usefull snippets
+      {"onsails/lspkind.nvim"} -- vs-code like pictograms
     }
   }
-  use { 'vim-airline/vim-airline' }
+  use {'vim-airline/vim-airline'}
 
   use {
     'nvim-tree/nvim-tree.lua',
@@ -69,14 +69,14 @@ return require('packer').startup(function(use)
     },
   }
 
-  use { 'christoomey/vim-tmux-navigator' }
-  use { 'prettier/vim-prettier' }
-  use { 'nvimtools/none-ls.nvim' }
-  use { 'nvim-lua/plenary.nvim' }
+  use {'christoomey/vim-tmux-navigator'}
+  use {'prettier/vim-prettier'}
+  use {'nvimtools/none-ls.nvim'}
+  use {'nvim-lua/plenary.nvim'}
   use {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    requires = { { "nvim-lua/plenary.nvim" } }
+    requires = { {"nvim-lua/plenary.nvim"} }
   }
   use({
     "stevearc/conform.nvim",
